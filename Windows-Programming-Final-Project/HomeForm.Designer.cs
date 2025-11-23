d80cc57145c1e0abe9e1534be01b54b73d535016
Rice-Wo <81062574+Rice-Wo@users.noreply.github.com>
2025-11-23 11:17:18 +0800

    重構命名空間並新增剪刀石頭布模組
    重構命名空間：
    - 將命名空間從 `Windows_Programming_Final_Project` 改為 `WindowsProgrammingFinalProject`，影響多個檔案，包括 `HomeForm`、`Program`、`Resources` 和 `Settings`。
    
    新增剪刀石頭布模組：
    - 新增 `RPS.cs` 類別。
    - 新增 `RPSForm.cs` 表單及其設計器檔案。
    - 更新 `Main.csproj`，加入新模組的編譯設定。
    
    更新表單設計：
    - 在 `RPSForm.Designer.cs` 中新增基本 UI 設計。

--- Windows-Programming-Final-Project/HomeForm.Designer.cs
+++ Windows-Programming-Final-Project/HomeForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Windows_Programming_Final_Project
+﻿namespace WindowsProgrammingFinalProject
 {
     partial class HomeForm
     {
--- Windows-Programming-Final-Project/Properties/Settings.Designer.cs
+++ Windows-Programming-Final-Project/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Windows_Programming_Final_Project.Properties
+namespace WindowsProgrammingFinalProject.Properties
 {
 
 
