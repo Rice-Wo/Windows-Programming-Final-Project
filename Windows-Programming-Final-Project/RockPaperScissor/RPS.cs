52dc8138d6884c64e63bc1490692039938ba2974
東吳電腦教室 <User@PC-18>
2025-12-02 14:51:30 +0800

    將剪刀石頭布的unitTest 寫完，這次直接用傳入測試方式以固定測試結果，也不需要測試隨機數

--- Windows-Programming-Final-Project/RockPaperScissor/RPS.cs
+++ Windows-Programming-Final-Project/RockPaperScissor/RPS.cs
@@ -5,29 +5,32 @@ namespace WindowsProgrammingFinalProject.RockPaperScissor
     public class RPS
     {
         public static readonly string[] input = { "石頭", "剪刀", "布" };
-        public RPS() { }
 
-        public string Game(string userInput, int? seed = null)
+        public string Game(string userInput, int? testInput = null)
         {
-            Random rand;
-            if (seed == null)
-            {
-               rand  = new Random();
-            }
-            else
-            {
-                rand = new Random((int)seed);
-            }
             int user = Array.IndexOf(input, userInput);
-            int computer = rand.Next(0, 3);
+            if (user == -1) {
+                Console.WriteLine("輸入異常");
+                return "輸入異常";
+            }
+            int computer;
+            if (testInput.HasValue) // for unit testing
+            {
+                computer = testInput.Value; 
+            }else
+            {
+                Random rand = new Random();
+                computer = rand.Next(0, 3);
+            }
+            
             int result = (user - computer + 3) % 3;
 
             string resultMessage = result switch
             {
                 0 => $"電腦出{input[computer]}，平手",
-                1 => $"電腦出{input[computer]}，你贏了",
-                2 => $"電腦出{input[computer]}，你輸了",
-                _ => $"電腦出{input[computer]}，未知結果"
+                1 => $"電腦出{input[computer]}，你輸了",
+                2 => $"電腦出{input[computer]}，你贏了",
+                _ => $"輸入異常"
             };
 
             Console.WriteLine(resultMessage);
